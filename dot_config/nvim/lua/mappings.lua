0f36a6caaecb90dc1797958688d1cc6cc7b096d0
mewxz029 <mewlawbreakertv@gmail.com>
2024-10-13 14:05:18 +0700

    reconfigure Rust for nvim with dap-ui

--- dot_config/nvim/lua/mappings.lua
+++ dot_config/nvim/lua/mappings.lua
@@ -8,18 +8,29 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 map("n", "<leader>ft", ":TodoTelescope<CR>")
 
-map("n", "<leader>db", "<cmd> DapToggleBreakpoint <CR>")
-map("n", "<leader>dus", function()
-  local widgets = require "dap.ui.widgets"
-  local sidebar = widgets.sidebar(widgets.scopes)
-  sidebar.open()
-end, { desc = "Open debugging sidebar" })
-
 map("v", "<leader>sc", function()
   require("nvim-silicon").clip()
 end, { desc = "Copy code screenshot to clipboard" })
 map("v", "<leader>sf", function()
   require("nvim-silicon").file()
 end, { desc = "Save code screenshot as file" })
+
+-- Nvim DAP
+map("n", "<Leader>dl", "<cmd>lua require'dap'.step_into()<CR>", { desc = "Debugger step into" })
+map("n", "<Leader>dj", "<cmd>lua require'dap'.step_over()<CR>", { desc = "Debugger step over" })
+map("n", "<Leader>dk", "<cmd>lua require'dap'.step_out()<CR>", { desc = "Debugger step out" })
+map("n", "<Leader>dc>", "<cmd>lua require'dap'.continue()<CR>", { desc = "Debugger continue" })
+map("n", "<Leader>db", "<cmd>lua require'dap'.toggle_breakpoint()<CR>", { desc = "Debugger toggle breakpoint" })
+map(
+  "n",
+  "<Leader>dd",
+  "<cmd>lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>",
+  { desc = "Debugger set conditional breakpoint" }
+)
+map("n", "<Leader>de", "<cmd>lua require'dap'.terminate()<CR>", { desc = "Debugger reset" })
+map("n", "<Leader>dr", "<cmd>lua require'dap'.run_last()<CR>", { desc = "Debugger run last" })
+
+-- rustaceanvim
+map("n", "<Leader>dt", "<cmd>lua vim.cmd('RustLsp testables')<CR>", { desc = "Debugger testables" })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
--- dot_config/nvim/lua/plugins/nvim-dap.lua
+++ dot_config/nvim/lua/plugins/nvim-dap.lua
@@ -2,39 +2,61 @@ return {
   {
     "mfussenegger/nvim-dap",
     config = function()
-      local dap = require "dap"
-      -- Adapters
-      -- C, C++, Rust
-      local codelldb = require("mason-registry").get_package("codelldb"):get_install_path() .. "/codelldb"
-      dap.adapters.codelldb = {
-        type = "server",
-        port = "${port}",
-        executable = {
-          command = codelldb,
-          args = { "--port", "${port}" },
-        },
-      }
-
-      dap.configurations.rust = {
-        {
-          name = "Launch",
-          type = "codelldb",
-          request = "launch",
-          program = function()
-            return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
-          end,
-          cwd = "${workspaceFolder}",
-          stopOnEntry = false,
-          targetArchitecture = "arm64",
-        },
-      }
+      local dap, dapui = require "dap", require "dapui"
+      dap.listeners.before.attach.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.launch.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated.dapui_config = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited.dapui_config = function()
+        dapui.close()
+      end
     end,
+    -- config = function()
+    --   local dap = require "dap"
+    --   -- Adapters
+    --   -- C, C++, Rust
+    --   local codelldb = require("mason-registry").get_package("codelldb"):get_install_path() .. "/codelldb"
+    --   dap.adapters.codelldb = {
+    --     type = "server",
+    --     port = "${port}",
+    --     executable = {
+    --       command = codelldb,
+    --       args = { "--port", "${port}" },
+    --     },
+    --   }
+    --
+    --   dap.configurations.rust = {
+    --     {
+    --       name = "Launch",
+    --       type = "codelldb",
+    --       request = "launch",
+    --       program = function()
+    --         return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+    --       end,
+    --       cwd = "${workspaceFolder}",
+    --       stopOnEntry = false,
+    --       targetArchitecture = "arm64",
+    --     },
+    --   }
+    -- end,
   },
   {
-    "theHamsta/nvim-dap-virtual-text",
-    lazy = false,
-    config = function(_, opts)
-      require("nvim-dap-virtual-text").setup()
+    "rcarriga/nvim-dap-ui",
+    dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
+    config = function()
+      require("dapui").setup()
     end,
   },
+  -- {
+  --   "theHamsta/nvim-dap-virtual-text",
+  --   lazy = false,
+  --   config = function(_, opts)
+  --     require("nvim-dap-virtual-text").setup()
+  --   end,
+  -- },
 }
