e77b8dc68587ab3384536c2c85e96bf27aa41b53
Wuttinan <wuttinan@TH-LW95175-MLC.local>
2025-02-27 18:00:32 +0700

    update neovim

--- dot_config/nvim/lua/plugins/copilot.lua
+++ dot_config/nvim/lua/plugins/copilot.lua
@@ -1,20 +1,14 @@
 return {
   {
-    "github/copilot.vim",
-    lazy = false,
-  },
-  {
     "CopilotC-Nvim/CopilotChat.nvim",
-    branch = "canary",
     dependencies = {
-      { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
-      { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+      { "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
+      { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
     },
     lazy = false,
     build = "make tiktoken", -- Only on MacOS or Linux
     opts = {
-      debug = true, -- Enable debugging
-      -- See Configuration section for rest
+      -- See Configuration section for options
     },
     -- See Commands section for default commands if you want to lazy load on them
   },
